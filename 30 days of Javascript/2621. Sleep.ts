84aceee8fadcdd7430ed43fddaab3c88e3622290
Arturs Torsters <arrturs.torsters@gmail.com>
2024-07-18 13:36:18 +0300

    resolve, reject , promise

--- 30 days of Javascript/2621. Sleep.ts
+++ 30 days of Javascript/2621. Sleep.ts
@@ -2,7 +2,10 @@
 //Given a positive integer millis, write an asynchronous function that sleeps for millis milliseconds. It can resolve any value.
 
 async function sleep(millis: number): Promise<void> {
-    
-}
-
-
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, millis);
+    });
+  }
+  
